64eab37ea095b20640e09ea3ae70860a9ea1901d
Sami Räihä <saraiha@helsinki.fi>
2025-10-16 19:39:24 +0300

    vaihe3 valmis

--- vaihe3/vaihe3.s
+++ vaihe3/vaihe3.s
@@ -1,26 +1,54 @@
 .data
-    tavut: .byte 1,2,3,4,5,6,7,8,9,10,0
+    tavut: .byte 1,2,3,4,5,6,7,8,9,0
+    .align 2
     sanat: .word 1,2,3,4,5,6,7,8,9,10,0
-    .text
-    .globl main
+    newline: .asciz "\n" 
+
+.text
+.globl main
 
 main:
-    la x9, tavut
-    li x20, 0
     jal ra, tavupituus
-
-    jal ra, sanapituus
-
     li a7, 10
+    ecall
 
 
 tavupituus:
-
+    la x9, tavut
+    li x20, 0
+    j loop_byte
 
 sanapituus:
+    la x9, sanat
+    li x20, 0
+    j loop_word
 
-.loop_byte:
+loop_byte:
     lb x10, 0(x9)
-    beq x10, x0, done
-
+    beq x10, x0, done_tavu
+    addi x20, x20, 1
+    addi x9, x9, 1
     j loop_byte
+
+loop_word:
+    lw x10, 0(x9)
+    beq x10, x0, done_sana
+    addi x20, x20, 1
+    addi x9, x9, 4
+    j loop_word
+
+done_tavu:
+    mv a0, x20
+    li a7, 1
+    ecall
+    li a7, 64
+    li a0, 1
+    la a1, newline
+    li a2, 1
+    ecall
+    j sanapituus
+
+done_sana:
+	mv a0, x20
+    li a7, 1
+    ecall
